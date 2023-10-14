a4c31530475a0113dac757d0ac8fe62dcf76064d
Aleksandr <savchenkov.alexandr2015@yandex.ru>
2023-10-14 22:09:25 +0300

    fix: Убран класс BaseController

--- src/main/java/ru/yandex/practicum/filmorate/Controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/FilmController.java
@@ -2,44 +2,61 @@ package ru.yandex.practicum.filmorate.Controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
-public class FilmController extends BaseController<Film> {
+public class FilmController {
     private static final LocalDate DATA_RELIES_MIN = LocalDate.of(1895,12,28);
+    private static long generationId = 0;
+    private final Map<Long, Film> storage = new HashMap<>();
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        super.making(film);
+        validate(film);
+        film.setId(generationIdUnit());
+        storage.put(film.getId(), film);
         log.info("The film was created {}",film);
             return film;
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        super.update(film);
-        log.info("The film was update {}",film);
+        validate(film);
+        if (storage.get(film.getId()) != null) {
+            storage.put(film.getId(), film);
+            log.info("The film was update {}",film);
+        } else {
+            log.warn("Exception, Data not found  id:{}", film.getId());
+            throw new DataNotFoundException("Data not found " + film.getId());
+        }
             return film;
     }
 
     @GetMapping
     public List<Film> getAllFilm() {
-        List<Film> list = super.getAll();
+        List<Film> list = new ArrayList<>(storage.values());
         log.info("The film was get all {}", list);
             return list;
     }
 
-    @Override
+
     public void validate(Film data) {
         if (data.getReleaseDate().isBefore(DATA_RELIES_MIN)) {
             log.warn("The film date is not correct {} ",data.getReleaseDate());
             throw new ValidationException("Invalid date" + data);
         }
+    }
+    private long generationIdUnit() {
+        return ++generationId;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/Controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/UserController.java
@@ -2,16 +2,23 @@ package ru.yandex.practicum.filmorate.Controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
-public class UserController extends BaseController<User> {
-    @Override
+public class UserController {
+
+    private static long generationId = 0;
+    private final Map<Long, User> storage = new HashMap<>();
+
     public void validate(User data) {
         if (data.getName() == null || data.getName().isBlank()) {
             data.setName(data.getLogin());
@@ -21,22 +28,34 @@ public class UserController extends BaseController<User> {
 
     @PostMapping
     public User createFilm(@Valid() @RequestBody User user) {
-        super.making(user);
+        validate(user);
+        user.setId(generationIdUnit());
+        storage.put(user.getId(), user);
         log.info("The user was created {}",user);
             return user;
     }
 
     @PutMapping
     public User updateFilm(@Valid @RequestBody User user) {
-        super.update(user);
-        log.info("The user was update {}",user);
+        validate(user);
+        if (storage.get(user.getId()) != null) {
+            storage.put(user.getId(), user);
+            log.info("The user was update {}",user);
+        } else {
+            log.warn("Exception, Data not found  id:{}", user.getId());
+            throw new DataNotFoundException("Data not found " + user.getId());
+        }
             return user;
     }
 
     @GetMapping
     public List<User> getAllFilm() {
-        List<User> list = super.getAll();
+        List<User> list = new ArrayList<>(storage.values());
         log.info("The film was get all {}", list);
-            return list;
+        return list;
+    }
+
+    private long generationIdUnit() {
+        return ++generationId;
     }
 }
