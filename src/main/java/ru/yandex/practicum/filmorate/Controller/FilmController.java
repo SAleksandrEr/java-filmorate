338d41f2da1b3f81fc6deca80014c045f5ebd6ba
Aleksandr <savchenkov.alexandr2015@yandex.ru>
2023-10-14 19:45:01 +0300

    fix: пробелы

--- src/main/java/ru/yandex/practicum/filmorate/Controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/FilmController.java
@@ -15,21 +15,21 @@ public class FilmController extends BaseController<Film> {
     private static final LocalDate DATA_RELIES_MIN = LocalDate.of(1895,12,28);
 
     @PostMapping
-    public Film createFilm(@Valid @RequestBody Film film){
+    public Film createFilm(@Valid @RequestBody Film film) {
         super.making(film);
         log.info("The film was created {}",film);
             return film;
     }
 
     @PutMapping
-    public Film updateFilm(@Valid @RequestBody Film film){
+    public Film updateFilm(@Valid @RequestBody Film film) {
         super.update(film);
         log.info("The film was update {}",film);
             return film;
     }
 
     @GetMapping
-    public List<Film> getAllFilm(){
+    public List<Film> getAllFilm() {
         List<Film> list = super.getAll();
         log.info("The film was get all {}", list);
             return list;
--- src/main/java/ru/yandex/practicum/filmorate/Controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/UserController.java
@@ -10,31 +10,31 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/users")
-public class UserController extends BaseController<User>{
+public class UserController extends BaseController<User> {
     @Override
     public void validate(User data) {
-        if (data.getName() == null || data.getName().isBlank()){
+        if (data.getName() == null || data.getName().isBlank()) {
             data.setName(data.getLogin());
             log.info("Display name is empty - login will be used - {} ", data.getName());
         }
     }
 
     @PostMapping
-    public User createFilm(@Valid() @RequestBody User user){
+    public User createFilm(@Valid() @RequestBody User user) {
         super.making(user);
         log.info("The user was created {}",user);
             return user;
     }
 
     @PutMapping
-    public User updateFilm(@Valid @RequestBody User user){
+    public User updateFilm(@Valid @RequestBody User user) {
         super.update(user);
         log.info("The user was update {}",user);
             return user;
     }
 
     @GetMapping
-    public List<User> getAllFilm(){
+    public List<User> getAllFilm() {
         List<User> list = super.getAll();
         log.info("The film was get all {}", list);
             return list;
