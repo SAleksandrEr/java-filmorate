0242e68fb2c63f52f62b197e867a4f441e1f7562
SAleksandrEr <134864147+SAleksandrEr@users.noreply.github.com>
2023-11-18 18:06:26 +0300

    Update ErrorHandler.java

--- src/main/java/ru/yandex/practicum/filmorate/Controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/ErrorHandler.java
@@ -39,7 +39,7 @@ public class ErrorHandler {
         return new ErrorResponse(e.getObjectName());
     }
 
-   @ExceptionHandler
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
         return new ErrorResponse("Произошла непредвиденная ошибка.");
